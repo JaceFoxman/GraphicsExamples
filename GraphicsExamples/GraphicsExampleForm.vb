7758ad05ab24619fded0f53b7dde9a1c2a74b8af
Jason Permann <JaceFoxman@gmail.com>
2025-03-31 08:51:28 -0600

    Add draw rectangle and circle

--- GraphicsExamples/GraphicsExampleForm.vb
+++ GraphicsExamples/GraphicsExampleForm.vb
@@ -11,17 +11,36 @@ Public Class GraphicsExampleForm
 
     Sub DrawLine()
         Dim g As Graphics = Me.CreateGraphics
-        Dim pen As New Pen(Color.Black)
+        Dim pen As New Pen(Color.Black, 3)
         'pen.Color = Color.Black
-        g.DrawLine(pen, 0, 0, 100, 100) 'Must have a defined pen starting x,y and ending x,y
+        g.DrawLine(pen, 0, 50, 100, 50) 'Must have a defined pen starting x,y and ending x,y
         g.Dispose()
     End Sub
 
-    Private Sub GraphicsExampleForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        DrawLine()
+    Sub DrawRectangle()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Red)
+        'pen.Color = Color.Black
+        g.DrawRectangle(pen, 150, 150, 300, 300) 'Must have a defined pen starting x,y and ending x,y
+        g.Dispose()
     End Sub
 
+    Sub DrawCircle()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Blue)
+        'pen.Color = Color.Black
+        g.DrawEllipse(pen, 150, 150, 300, 300) 'Must have a defined pen starting x,y and ending x,y
+        g.Dispose()
+    End Sub
+
+    'Event Handelers-------------------------------------------------------------------------------------
+    Private Sub GraphicsExampleForm_Click(sender As Object, e As EventArgs) Handles Me.Click
+        DrawLine()
+        DrawRectangle()
+        DrawCircle()
+    End Sub
     Private Sub GraphicsExampleForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
         Me.Text = $"({e.X}, {e.Y})"
+
     End Sub
 End Class
